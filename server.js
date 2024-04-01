64851ea0ba41611be7c1fbaf4b8a9bfaed86c054
Josh Massa <joshmassa@gmail.com>
2024-03-31 20:46:56 -0400

    Added connection.js, started building file structure, added BlogPost model, created skeleton main.handlebars

--- server.js
+++ server.js
@@ -1,55 +1,55 @@
 // Import required packages
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
 const mysql = require('mysql2');
 require('dotenv').config();
 
 
 // Import sequelize and initialize session to store session data
-const sequelize = require('sequelize');
-const SequelizeStore = require('connect-session-sequelize');
+// const sequelize = require('sequelize');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Initialize app and define PORT
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Import secret from .env and configure a session
-const secret = process.env.SESSION_SECRET;
+// const secret = process.env.SESSION_SECRET;
 
-const sess = {
-    secret,
-    cookie: {
-      maxAge: 300000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+// const sess = {
+//     secret,
+//     cookie: {
+//       maxAge: 300000,
+//       httpOnly: true,
+//       secure: false,
+//       sameSite: 'strict',
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize
+//     })
+//   };
 
 // Set up session management middleware
-app.use(sess);
+// app.use(sess);
 
 // Set the view engine to handlebars and set default layout to main
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Tell Express to use handlebars as the view engine
-app.set('view engine', 'handlebars');
+// app.set('view engine', 'handlebars');
 
 // Import necessary middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 // Synchronize Sequelize models with the database and start the server
-sequelize.sync({ force: false }).then(() => {
+// sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App now listening on port ${PORT}`));
-});
+// });
